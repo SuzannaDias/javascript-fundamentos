2a3fff5dafdeb5721a6847a5507ac76183dce3fc
SuzannaDias <Suzanna.dias77@gmail.com>
2026-06-23 10:55:21 -0300

    Refactor array manipulation and logging in arrays.js

--- arrays.js
+++ arrays.js
@@ -1,5 +1,25 @@
-var nomes = ['Ana', 'João', 'Maria', 'Pedro']
+// Exemplo de manipulação de arrays
 
-for (var pos in nomes) {
-    console.log(`O nome ${nomes[pos]} está na posição ${pos}`)
-}
+let num = [5, 8, 2, 9, 3]
+
+// Adiciona um novo valor ao final do vetor
+num.push(1)
+
+// Organiza os valores em ordem crescente
+num.sort()
+
+// Substitui o valor da posição 4
+num[4] = 3
+
+console.log(num)
+
+console.log(`O valor da posição 3 é ${num[3]}`)
+
+console.log(`O vetor tem ${num.length} posições`)
+
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+
+// Procura a posição do valor 3
+let pos = num.indexOf(3)
+
+console.log(`O valor 3 está na posição ${pos}`)
